d169885655096f72fa785c84de3986ca201c3070
jonchang03 <jonchang03@gmail.com>
2017-01-12 22:32:27 -0500

    quiz 2 part 2

--- rprogramming/complete.R
+++ rprogramming/complete.R
@@ -13,4 +13,11 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
+    
+    nobs <- numeric()
+    for (i in id) {
+        readFiles <- read.csv(paste(directory, "/", formatC(i, width=3, flag="0"), ".csv", sep=""))
+        nobs <- c(nobs, sum(complete.cases(readFiles)))
+    }
+    data.frame(id, nobs)
 }
---
+++ rprogramming/corr.R
@@ -0,0 +1,4 @@
+## Part 3
+corr <- function(directory, threshold = 0) {
+    
+}
