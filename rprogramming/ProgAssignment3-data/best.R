5e9278f4a291309a572ea4e34ffb0b053c446cad
jonchang03 <jonchang03@gmail.com>
2017-01-16 21:52:39 -0500

    Assignment 3 Part 2

---
+++ rprogramming/ProgAssignment3-data/best.R
@@ -0,0 +1,30 @@
+best <- function(state, outcome) {
+    ## Read outcome data
+    df <- read.csv("outcome-of-care-measures.csv")
+    
+    ## Check that state and outcome are valid
+    if(!(state %in% df$State)) {
+        stop("invalid state")
+    }
+    if(outcome == "heart attack") {
+        i <- 11
+    } else if (outcome == "heart failure") {
+        i <- 17
+    } else if (outcome == "pneumonia") {
+        i <-23
+    } else {
+        stop("invalid outcome")
+    }
+    
+    ## Return hospital name in that state with lowest 30-day death rate
+    data <- subset(df, select=c(2,7,i)) # keep only the relevant columns
+    data <- subset(data, state==data$State) # keep only relevant rows
+    data[,3] <- suppressWarnings(as.numeric(as.character(data[,3]))) # convert "Not Available" to NAs
+    data <- data[complete.cases(data),] # get rid of NAs
+    
+    # get list of hospitals with lowest 30-day death
+    hospitals <- data[(data[,3] == min(data[,3])), 1]
+    
+    # break ties
+    paste(sort(hospitals)[1])
+}
