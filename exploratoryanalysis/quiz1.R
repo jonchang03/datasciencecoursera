87bec54f0ba45bae38e1a9c9d772d9c8f241f288
Jonathan Chang <jonchang03@gmail.com>
2017-01-31 21:23:50 -0500

    finished quiz 1 for Exploratory Data Analysis

---
+++ exploratoryanalysis/quiz1.R
@@ -0,0 +1,44 @@
+## Week 1 Quiz
+
+## Question 1
+# Which of the following is a principle of analytic graphics?
+# Describe and document the evidence
+
+## Question 2
+# What is the role of exploratory graphs in data analysis?
+# They are typically made very quickly.
+
+## Question 3
+# Which of the following is true about the base plotting system?
+# Plots are created and annotated with separate functions
+# Functions like 'plot' or 'hist' typically create the plot on the graphics device 
+# and functions like 'lines', 'text', or 'points' will annotate or add data to the plot.
+
+## Question 4
+# Which of the following is an example of a valid graphics device in R?
+# The computer screen
+
+## Question 5
+# Which of the following is an example of a vector graphics device in R?
+# Postscript
+
+## Question 6
+# Bitmapped file formats can be most useful for
+# Scatterplots with many many points
+
+
+## Question 7
+# Which of the following functions is typically used to add elements to a plot in the base graphics system?
+lines()
+
+## Question 8
+# Which function opens the screen graphics device for the Mac?
+# quartz()
+
+## Question 9
+# What does the 'pch' option to par() control?
+# the plotting symbol/character in the base graphics system
+
+## Question 10
+# If I want to save a plot to a PDF file, which of the following is a correct way of doing that?
+# Construct the plot on the screen device and then copy it to a PDF file with dev.copy2pdf()
